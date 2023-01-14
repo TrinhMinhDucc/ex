dc3540c79b71dd6853d6266d65036e9b8b6ccb20
TrinhMinhDucc <112499453+TrinhMinhDucc@users.noreply.github.com>
2023-01-14 11:59:06 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,2 +1,2 @@
-docker run -d --restart unless-stopped --pull always --name a1 rg.fr-par.scw.cloud/exorde-labs/exorde-cli -m 0x3511E536Db23341B5d4Ea505E12ca06e1FAB5BF4 -l 2
-docker run -d --restart unless-stopped --pull always --name a2 rg.fr-par.scw.cloud/exorde-labs/exorde-cli -m 0x3511E536Db23341B5d4Ea505E12ca06e1FAB5BF4 -l 2
+docker run -d --restart unless-stopped --pull always --name a1 exordelabs/exorde-cli -m 0x3511E536Db23341B5d4Ea505E12ca06e1FAB5BF4 -l 2
+docker run -d --restart unless-stopped --pull always --name a2 exordelabs/exorde-cli -m 0x3511E536Db23341B5d4Ea505E12ca06e1FAB5BF4 -l 2
